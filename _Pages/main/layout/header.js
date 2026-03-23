bd1f71dc09f5c91f3b4af1567e25984a5a57bca1
Your Name <you@example.com>
2026-03-23 03:37:50 -0500

    100%

--- _Pages/main/layout/header.js
+++ _Pages/main/layout/header.js
@@ -1,6 +1,4 @@
 "use client"
-import { apiFetch } from "@/_EXTRAS/peticion"
-
 import { useState, useEffect } from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
